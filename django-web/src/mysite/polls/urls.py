4a2fb2c1d1cc0e08f337f55b52487b7d64719658
repllca <maridann1022@icloud.com>
2024-05-15 06:02:45 +0900

    #1 viewの追加

--- django-web/src/mysite/polls/urls.py
+++ django-web/src/mysite/polls/urls.py
@@ -6,4 +6,5 @@ from . import views
 # ここに書いたurlが画面に表示される？
 urlpatterns = [
     path("",views.index,name = "index"),
+    path("test/",views.test)
 ]
--- django-web/src/mysite/polls/views.py
+++ django-web/src/mysite/polls/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello,world. youre at the polls index.")
 
+
+#シンプルな表示（htmlの表示）
+def test(request):
+    html = "<h1>aaa</h1><h2>aaa</h2>"
+    return HttpResponse(html)
