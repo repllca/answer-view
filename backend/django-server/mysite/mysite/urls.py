491f61f609ac011d1c093ab433943c312889c5aa
soto <syuweb522@gmail.com>
2024-05-25 19:14:52 +0900

    認証機能の追加

--- backend/django-server/mysite/mysite/urls.py
+++ backend/django-server/mysite/mysite/urls.py
@@ -4,4 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),#database
     path("myapp/",include("myapp.urls")),#
+    path("api-auth/",include("rest_framework.urls")),
 ]
