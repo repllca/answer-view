3aff33ebefaadd8bea8a7b14f11cecbb52e739c4
soto <syuweb522@gmail.com>
2024-07-02 20:20:02 +0900

    #4 jwt認証の追加

--- backend/django-server/mysite/myapp/urls.py
+++ backend/django-server/mysite/myapp/urls.py
@@ -3,11 +3,8 @@ from .views import TitleListAPIView, KadaiCreateAPIView, KadaiDetailAPIView
 from rest_framework.routers import DefaultRouter
 
 
-router = DefaultRouter()
-router.register(r"kadai",KadaiCreateAPIView)
-
 urlpatterns = [
     path("kadai/list/",TitleListAPIView.as_view(),name="Kadai-List"),
     path("kadai/<int:pk>/detail/",KadaiDetailAPIView.as_view(),name="Kadai-Detail"),
-    path("",include(router.urls)),
+    path("kadai/<int:pk>/create/",KadaiCreateAPIView.as_view()),
 ]
--- backend/django-server/mysite/myapp/views.py
+++ backend/django-server/mysite/myapp/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets, generics, status, permissions, filters
-from django_filters.rest_framework import DjangoFilterBackend
 from .models import Kadai
 from .serializers import KadaiSerializer, TitleListSerializer
 from .permissions import IsOwnerOrReadOnly
@@ -8,14 +7,13 @@ class TitleListAPIView(generics.ListAPIView):
     """
     動作:保存されているリストを表示
     permission:誰でも
-    Threadの削除はadminuserが行うべき
     """
     queryset = Kadai.objects.all().order_by("-date")
     serializer_class = TitleListSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ["title", "owner", "date"]
 
-class KadaiCreateAPIView(viewsets.ModelViewSet):
+class KadaiCreateAPIView(generics.RetrieveUpdateDestroyAPIView):
     """
     動作:kadaiの作成を行う
     permission:ログインユーザーだけ作成可能
@@ -30,7 +28,7 @@ class KadaiCreateAPIView(viewsets.ModelViewSet):
 class KadaiDetailAPIView(generics.RetrieveAPIView):
     """
     動作:titleの詳細表示を行う
-    permission:ログインユーザだけ
+    permission:ログインユーザだけ,編集が可能
     """
     queryset = Kadai.objects.all().order_by("-date")
     serializer_class = KadaiSerializer
